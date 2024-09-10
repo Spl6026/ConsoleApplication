a34bc413bd48589e0a7b1d569f87b1894dd153b3
spl6026 <paullin123456789@gmail.com>
2024-09-10 23:17:11 +0800

    upload

---
+++ Work/Work/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.score1 = new System.Windows.Forms.Label();
+            this.score2 = new System.Windows.Forms.Label();
+            this.content1 = new System.Windows.Forms.Label();
+            this.content2 = new System.Windows.Forms.Label();
+            this.mask = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Pillar = new System.Windows.Forms.PictureBox();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.Player1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mask)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pillar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // score1
+            // 
+            this.score1.AutoSize = true;
+            this.score1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.score1.Location = new System.Drawing.Point(10, 10);
+            this.score1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.score1.Name = "score1";
+            this.score1.Size = new System.Drawing.Size(0, 16);
+            this.score1.TabIndex = 6;
+            // 
+            // score2
+            // 
+            this.score2.AutoSize = true;
+            this.score2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.score2.Location = new System.Drawing.Point(460, 10);
+            this.score2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.score2.Name = "score2";
+            this.score2.Size = new System.Drawing.Size(0, 16);
+            this.score2.TabIndex = 7;
+            // 
+            // content1
+            // 
+            this.content1.AutoSize = true;
+            this.content1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.content1.Location = new System.Drawing.Point(176, 100);
+            this.content1.Name = "content1";
+            this.content1.Size = new System.Drawing.Size(228, 135);
+            this.content1.TabIndex = 9;
+            this.content1.Text = "阿ㄆ一ㄚˇ打排球\r\n\r\n\r\n\r\n按空格鍵開始";
+            this.content1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // content2
+            // 
+            this.content2.AutoSize = true;
+            this.content2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.content2.Location = new System.Drawing.Point(134, 250);
+            this.content2.Name = "content2";
+            this.content2.Size = new System.Drawing.Size(311, 48);
+            this.content2.TabIndex = 10;
+            this.content2.Text = "Player1以A, S左右移動 W跳躍 Z擊球\r\n\r\nPlayer2以左, 右鍵左右移動 上跳躍 Enter擊球";
+            this.content2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mask
+            // 
+            this.mask.Location = new System.Drawing.Point(0, 0);
+            this.mask.Name = "mask";
+            this.mask.Size = new System.Drawing.Size(580, 370);
+            this.mask.TabIndex = 8;
+            this.mask.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Transparent;
+            this.Ball.Image = global::Work.Properties.Resources.Ball;
+            this.Ball.Location = new System.Drawing.Point(280, 100);
+            this.Ball.Margin = new System.Windows.Forms.Padding(2);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(40, 40);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ball.TabIndex = 3;
+            this.Ball.TabStop = false;
+            // 
+            // Pillar
+            // 
+            this.Pillar.BackColor = System.Drawing.Color.Transparent;
+            this.Pillar.Image = global::Work.Properties.Resources.Pillar;
+            this.Pillar.Location = new System.Drawing.Point(290, 200);
+            this.Pillar.Margin = new System.Windows.Forms.Padding(2);
+            this.Pillar.Name = "Pillar";
+            this.Pillar.Size = new System.Drawing.Size(20, 160);
+            this.Pillar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pillar.TabIndex = 2;
+            this.Pillar.TabStop = false;
+            // 
+            // Player2
+            // 
+            this.Player2.BackColor = System.Drawing.Color.Transparent;
+            this.Player2.Image = global::Work.Properties.Resources.Player;
+            this.Player2.Location = new System.Drawing.Point(454, 300);
+            this.Player2.Margin = new System.Windows.Forms.Padding(2);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(60, 64);
+            this.Player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player2.TabIndex = 1;
+            this.Player2.TabStop = false;
+            // 
+            // Player1
+            // 
+            this.Player1.BackColor = System.Drawing.Color.Transparent;
+            this.Player1.Image = global::Work.Properties.Resources.Player;
+            this.Player1.Location = new System.Drawing.Point(69, 300);
+            this.Player1.Margin = new System.Windows.Forms.Padding(2);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(60, 64);
+            this.Player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player1.TabIndex = 0;
+            this.Player1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.content2);
+            this.Controls.Add(this.content1);
+            this.Controls.Add(this.mask);
+            this.Controls.Add(this.score2);
+            this.Controls.Add(this.score1);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Pillar);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "阿ㄆ一ㄚˇ打排球";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.mask)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pillar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player1;
+        private System.Windows.Forms.PictureBox Player2;
+        private System.Windows.Forms.PictureBox Pillar;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label score1;
+        private System.Windows.Forms.Label score2;
+        private System.Windows.Forms.PictureBox mask;
+        private System.Windows.Forms.Label content1;
+        private System.Windows.Forms.Label content2;
+    }
+}
+
---
+++ Work/Work/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Work
+{
+    
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer bounce = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer bgm = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer hit = new WMPLib.WindowsMediaPlayer();
+        
+        bool start = false;
+
+        bool goleft = false;
+        bool goright = false;
+        bool jumping = false;
+        bool move = false;
+
+        int jumpSpeed = 0;
+        int force = 0;
+
+
+        bool goleft1 = false;
+        bool goright1 = false;
+        bool jumping1 = false;
+        bool move1 = false;
+
+        int jumpSpeed1 = 0;
+        int force1 = 0;
+
+        int ballx = 0;
+        int bally = 0;
+
+        int point = 0;
+
+        int player1score=0, player2score=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bgm.settings.setMode("loop", true);
+            bgm.URL = "start.mp3";
+            bgm.controls.play();
+            mask.Visible = true;
+            content1.Visible = true;
+            content2.Visible = true;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            score1.Text = "Player1得分 = " + player1score;
+            score2.Text = "Player2得分 = " + player2score;
+
+
+
+
+            if (player1score >= 15)
+                gameOver("Player1");
+
+            if (player2score >= 15)
+                gameOver("Player2");
+
+
+
+
+            if (Player1.Left < 0)
+                Player1.Left = 0;
+
+            if (Player1.Right + 10 > Pillar.Left)
+                Player1.Left = Pillar.Left - 70;
+
+            if (Player2.Right > ClientSize.Width)
+                Player2.Left = ClientSize.Width - 60;
+
+            if (Player2.Left - 10 < Pillar.Right)
+                Player2.Left = Pillar.Right + 10;
+
+
+
+
+            if (goleft) 
+                Player1.Left -= 15;
+
+            if (goright) 
+                Player1.Left += 15;
+
+            if (jumping && force != 10)
+            {
+                jumpSpeed = -20;
+                force += 1;
+                move = true;
+            }
+            else
+            {
+                jumpSpeed = 20;
+                if (Player1.Top > 290)
+                {
+                    force = 0;
+                    jumpSpeed = 0;
+                    jumping = false;
+                    move = false;
+                }
+            }
+            if(move)
+                Player1.Top += jumpSpeed;
+
+
+
+
+            if (goleft1) 
+                Player2.Left -= 15; 
+            
+            if (goright1)
+                 Player2.Left += 15;
+            
+            if (jumping1 && force1 != 10)
+            {
+                jumpSpeed1 = -20;
+                force1 += 1;
+                move1 = true;
+            }
+            else
+            {
+                jumpSpeed1 = 20;
+                if (Player2.Top > 290)
+                {
+                    force1 = 0;
+                    jumpSpeed1 = 0;
+                    jumping1 = false;
+                    move1 = false;
+                }
+            }
+            if (move1)
+                Player2.Top += jumpSpeed1;
+
+
+
+
+            if (Ball.Bounds.IntersectsWith(Player1.Bounds))
+            {
+                bounce.URL = "bounce.mp3";
+                bounce.controls.play();
+                jumping = false;
+                ballx = 1;
+                bally = -20;
+            }
+
+            if (Ball.Bounds.IntersectsWith(Player2.Bounds))
+            {
+                bounce.URL = "bounce.mp3";
+                bounce.controls.play();
+                jumping1 = false;
+                ballx = -1;
+                bally = -20;
+            }
+
+            if (Ball.Bounds.IntersectsWith(Pillar.Bounds))
+            {
+                bounce.URL = "bounce.mp3";
+                bounce.controls.play();
+                if (Ball.Bottom - 10 > Pillar.Top) 
+                    ballx = -ballx;
+
+                else
+                    bally = -bally;
+            }
+
+
+
+
+            if (Ball.Top <= 0)
+            {
+                bounce.URL = "bounce.mp3";
+                bounce.controls.play();
+                bally = -bally;
+            }
+
+            if (Ball.Left <= 0 || Ball.Right >= ClientSize.Width)
+            {
+                bounce.URL = "bounce.mp3";
+                bounce.controls.play();
+                ballx = -ballx;
+            }
+
+            if (Ball.Top + Ball.Height > ClientSize.Height)
+            {
+                jumping= false;
+                jumping1= false;
+                Player1.Top = 300;
+                Player2.Top = 300;
+                Player1.Left = 70;
+                Player2.Left = ClientSize.Width - 130;
+                if (Ball.Left > 280)
+                {
+                    player1score = player1score + 1;
+                    point = 1;
+                }
+
+                else
+                {
+                    player2score = player2score + 1;
+                    point = 2;
+                }
+                if (point == 1)
+                {
+                    Ball.Location = new Point(Player2.Left + 10, 0);
+                    ballx = 0;
+                    point = 0;
+                }
+                else if (point == 2)
+                {
+                    Ball.Location = new Point(Player1.Left + 10, 0);
+                    ballx = 0;
+                    point = 0;
+                }
+                bally = 5;
+            }
+
+            bally += 1;
+
+            Ball.Left += ballx;
+            Ball.Top += bally;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                goleft = true;
+
+            if (e.KeyCode == Keys.D)
+                goright = true;
+
+            if (e.KeyCode == Keys.W && !jumping) 
+                jumping = true;
+
+            if (e.KeyCode == Keys.Z)
+            {
+                int X = Player1.Right - Ball.Right;
+                int Y = Player1.Top - Ball.Bottom;
+                if ((X < 50 && X > -50) && (Y < 50 && Y > -50))
+                {
+                    hit.URL = "hit.mp3";
+                    hit.controls.play();
+                    ballx = 20;
+                    bally = 5;
+                }
+            }
+
+
+            if (e.KeyCode == Keys.Left)
+                goleft1 = true; 
+            
+            if (e.KeyCode == Keys.Right)
+                goright1 = true;
+            
+            if (e.KeyCode == Keys.Up && !jumping1)
+                jumping1 = true; 
+            
+            if (e.KeyCode == Keys.Enter)
+            {
+                int X = Player2.Left - Ball.Left;
+                int Y = Player2.Top - Ball.Bottom;
+                if ((X < 50 && X > -50) && (Y < 50 && Y > -50))
+                {
+                    hit.URL = "hit.mp3";
+                    hit.controls.play();
+                    ballx = -20;
+                    bally = 5;
+                }
+            }
+
+            if (e.KeyCode == Keys.Space && !start)
+            {
+                bgm.URL = "bgm.mp3";
+                bgm.controls.play();
+                mask.Visible = false;
+                content1.Visible = false;
+                content2.Visible = false;
+                timer1.Start();
+                start = true;
+            }
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                goleft = false;
+            
+            if (e.KeyCode == Keys.D)
+                goright = false;
+
+
+            if (e.KeyCode == Keys.Left)
+                goleft1 = false;
+
+            if (e.KeyCode == Keys.Right) 
+                goright1 = false;
+        }
+
+        private void gameOver(string s)
+        {
+            timer1.Stop();
+            DialogResult dr = MessageBox.Show(s + "勝利" + "\n" + "遊戲結束" + "\n" + "按確定重新開始，按取消退出", "遊戲結束", MessageBoxButtons.OKCancel);
+            if (dr == DialogResult.OK)
+                Application.Restart();
+            else if (dr == DialogResult.Cancel)
+                Application.Exit();
+        }
+    }
+}
